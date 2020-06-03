0f2669d9565cfdc3aba0c657b74f3c483479211c
pawan deore <61040146+pavandeore@users.noreply.github.com>
2020-06-03 23:32:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     const fetchUrl = `https://picsum.photos/id/${id_no}/info`;
     console.log(fetchUrl);
        $.get(fetchUrl,function(data){
-            $(".card-text").append(data.author);
+            $(".A-text").append(data.author);
             $(".A-id").append(data.id);
             $("#newUrl").attr("href", data.download_url); 
             $("#card-img").attr("src", data.url); 
