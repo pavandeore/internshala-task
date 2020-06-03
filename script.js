0fa3824e84abb9a323dd7e202149915611c81d37
pawan deore <61040146+pavandeore@users.noreply.github.com>
2020-06-03 18:06:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    // let setUrl = document.querySelector(".download-btn");
+    // console.log(setUrl);
+    let id_no = Math.round(Math.random()*500);
+    const fetchUrl = `https://picsum.photos/id/${id_no}/info`;
+    console.log(fetchUrl);
+       $.get(fetchUrl,function(data){
+            $(".card-text").append(data.author);
+            $("#newUrl").attr("href", data.download_url); 
+            $(".card-text").append(data.url); 
+       })  
+           
+})
